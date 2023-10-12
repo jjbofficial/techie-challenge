991197ed1f92aa0f48cee7e72b7d1cf31da78c15
Jeremiah John Boateng <jeremiahjohnboateng@gmail.com>
2023-10-12 19:17:00 +0000

    enable access to docs in all environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('viewApiDocs', function (User $user) {
+        Gate::define('viewApiDocs', function (?User $user) {
             return true;
         });
 
